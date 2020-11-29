4bdf0647b2d6bf8aa2bdd7cc21b31d31d160e8e7
rekeye <12szymon.paluch34@gmail.com>
2020-11-29 15:15:53 +0100

    third-commit

--- js/inputEnforcer.js
+++ js/inputEnforcer.js
@@ -1,6 +1,10 @@
-document.querySelectorAll("input").forEach(el => el.addEventListener('keyup', () => {
+document.querySelectorAll("input").forEach(el => el.addEventListener('keydown', () => {
     const max = parseInt(el.max);
     const min = parseInt(el.min);
-    if (el.value > max) el.value = max;
-    if (el.value < min) el.value = min;
+    if (el.value === "") {
+        return;
+    } else {
+        if (el.value > max) el.value = max;
+        if (el.value < min) el.value = min;
+    }
 }))
